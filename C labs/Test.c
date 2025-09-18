c45170d6b395b3328bf7fa0f8caa5fba5ea61df5
Harry <staha023@student.otago.ac.nz>
2025-09-19 10:57:45 +1200

    L

---
+++ C labs/Test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+    int i = 42;
+    char z = 'c';
+    double d = 3.14;
+    
+    printf("The integer is %d, the char is %c and the double is %f", i, z, d);
+}
